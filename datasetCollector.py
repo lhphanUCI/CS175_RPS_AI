0996e1fccbb1760bd5761065211934fce503c661
Kyle Ingwersen <kingwersen@outlook.com>
2018-05-24 09:48:29 -0700

    Prevent array thrashing in FPSCounter

--- datasetCollector.py
+++ datasetCollector.py
@@ -106,15 +106,18 @@ class SimpleFPSCounter:
 
     def __init__(self, length=5):
         self.last_time = timer()
-        self.last_intervals = []
+        self.last_intervals = [0] * length
         self.length = length
+        self.counter = 0
 
     def update(self):
         new_time = timer()
-        self.last_intervals.append(new_time - self.last_time)
+        self.last_intervals[self.counter] = new_time - self.last_time
         self.last_time = new_time
-        if len(self.last_intervals) > self.length:
-            self.last_intervals.pop(0)
+        if self.counter < self.length - 1:
+            self.counter += 1
+        else:
+            self.counter = 0
         return 1 / mean(self.last_intervals)
 
 
