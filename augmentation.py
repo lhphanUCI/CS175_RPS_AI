0d9ee9af6a657550c6001da616ebc1ba31e3aaa1
Lee Phan <lhphan@uci.edu>
2018-06-01 16:41:39 -0700

    Modified parameters

--- augmentation.py
+++ augmentation.py
@@ -4,6 +4,7 @@ import numpy as np
 import re
 import argparse
 import json
+import loadDataset
 
 # From https://www.pyimagesearch.com/2015/10/05/opencv-gamma-correction/
 
@@ -20,7 +21,7 @@ def adjust_gamma(image, gamma=1.0):
 	# apply gamma correction using the lookup table
 	return cv2.LUT(image, table)
     
-def getBrightnessAugForSingleMove(moveFramesDirPath:str, moveCSVPath:str, gamma:float)->('[X]','[Y]'):
+def getBrightnessAug(X:'nparry', gamma:float)->'nparry':
     global SETTINGS_PATH
     global TEST_MODE
     
@@ -40,52 +41,31 @@ def getBrightnessAugForSingleMove(moveFramesDirPath:str, moveCSVPath:str, gamma:
         if not os.path.exists(outputTestVideosDir): #Creates outputTestVideos folder
             os.makedirs(outputTestVideosDir)
             
-        origOutput = cv2.VideoWriter(outputTestVideosDir + "/orig" + os.path.basename(moveCSVPath) + '.avi', fourcc, frameRate, outputDim)
-        augOutput = cv2.VideoWriter(outputTestVideosDir + "/aug" + os.path.basename(moveCSVPath) + '.avi', fourcc, frameRate, outputDim)
+        origOutput = cv2.VideoWriter(outputTestVideosDir + '/orig.avi', fourcc, frameRate, outputDim)
+        augOutput = cv2.VideoWriter(outputTestVideosDir + '/aug.avi', fourcc, frameRate, outputDim)
         
-    fileList = os.listdir(moveFramesDirPath)
-    moveX = []
-    amtFiles = len(fileList)
-    for i in range (1, amtFiles + 1):
-        fullPath = moveFramesDirPath + "/" + str(i) + ".jpeg"
-        img = cv2.imread(fullPath,cv2.IMREAD_COLOR )
-        resizedImg = cv2.resize(img, (64, 64))
-        resizedAugImg = adjust_gamma(resizedImg, gamma)
-        resizedAugRgbFrame = cv2.cvtColor(resizedAugImg, cv2.COLOR_BGR2RGB)  # Now in [r, b, g] form
-        moveX.append(resizedAugRgbFrame)
+    XAug = []
+    for i in range(len(X)):
+        rgbImg = X[i]
+        augRgbImg = adjust_gamma(rgbImg, gamma)
+        XAug.append(augRgbImg)
 
         if TEST_MODE:
-            origOutput.write(resizedImg)
-            augOutput.write(resizedAugImg)
+            origBGRImg = cv2.cvtColor(rgbImg, cv2.COLOR_RGB2BGR)  # Now in [b, g, r] form
+            augBGRImg = cv2.cvtColor(augRgbImg, cv2.COLOR_RGB2BGR)  # Now in [b, g, r] form
+            origOutput.write(origBGRImg)
+            augOutput.write(augBGRImg)
 
     if TEST_MODE:
         origOutput.release()
         augOutput.release()
-    moveX = np.asarray(moveX)
-
-    moveY = np.zeros(amtFiles)
-    i=0
-    with open(moveCSVPath) as f:
-        for line in f:
-            moveY[i] = int(line)
-            i = i + 1
-
-    return (moveX, moveY)
-
-def getAugmentedBrightnessDataSet(rockFramesDirPath:str, paperFramesDirPath:str, scissorFramesDirPath:str
-                , rockCSVPath:str, paperCSVPath:str, scissorCSVPath:str, gamma:float)->('[X]','[Y]'):
-
-    rockX, rockY = getBrightnessAugForSingleMove(rockFramesDirPath, rockCSVPath, gamma)
-    paperX, paperY = getBrightnessAugForSingleMove(paperFramesDirPath, paperCSVPath, gamma)
-    scissorX, scissorY = getBrightnessAugForSingleMove(scissorFramesDirPath, scissorCSVPath, gamma)
-    
-    X = np.concatenate((rockX, paperX, scissorX), axis=0)
-    Y = np.concatenate((rockY, paperY, scissorY), axis=0)
-
-    return (X, Y)   
-
+        
+    XAug = np.asarray(XAug)
+    return XAug
      
 if __name__ == "__main__":
-    gamma = 0.5 # Value of 1 means original output. Between (0, 1) means darken. Above 1 means brighten
-    X, Y = getAugmentedBrightnessDataSet('./dataset/imgs/rock_frames', './dataset/imgs/paper_frames', './dataset/imgs/scissor_frames'
-                , './dataset/csvs/rock.csv', './dataset/csvs/paper.csv', './dataset/csvs/scissor.csv', gamma)
+    gamma = 0.6 # Value of 1 means original output. Between (0, 1) means darken. Above 1 means brighten
+    X, Y = loadDataset.loadDataSet('./dataset/imgs/rock_frames', './dataset/imgs/paper_frames', './dataset/imgs/scissor_frames'
+                , './dataset/csvs/rock.csv', './dataset/csvs/paper.csv', './dataset/csvs/scissor.csv')
+    XAug = getBrightnessAug(X, gamma)
+    
